09abfc4cd6853920a8c6a6708c1754175ae9b235
Alexandr Yerko <casha-e00@gmail.com>
2021-04-12 17:06:06 +0300

    Sixth homework has been added

---
+++ 006_functions_2/006_homework_1.py
@@ -0,0 +1,10 @@
+
+# Задание 2
+
+def palindrom(p):
+	if wwrite == "".join(reversed(wwrite)):
+		return True
+	return False
+write = str(input("Введите любую фразу: "))
+wwrite = write.replace(" ", "").replace(",", "")
+print(palindrom(write))
